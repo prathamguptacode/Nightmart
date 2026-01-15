24a8780b696b2b323285fde0fc7a5bf1156c36e5
Pratham Gupta <prathamgupta.wk@gmail.com>
2026-01-15 13:23:05 +0530

    complete backend

--- server/src/controller/product.ts
+++ server/src/controller/product.ts
@@ -7,11 +7,11 @@ export async function listProduct(req: Request, res: Response) {
     if (!req.body) {
         return res.status(400).json({ message: 'Body not found' });
     }
-    const { name, description, price } = req.body;
-    if (!name || !description || !price) {
+    const { name, description, price, tags } = req.body;
+    if (!name || !description || !price || !tags) {
         return res.status(400).json({ message: 'Invalid inputs' });
     }
-
+    const tagArray = tags.split(' ');
     let userId: {
         id: number;
     } | null;
@@ -38,6 +38,7 @@ export async function listProduct(req: Request, res: Response) {
                 description: description,
                 price: price,
                 userId: userId.id,
+                tags: tagArray,
             },
             select: {
                 name: true,
@@ -61,23 +62,90 @@ export async function seeProducts(req: Request, res: Response) {
     if (!req.body) {
         return res.status(400).json({ message: 'Body not found' });
     }
-    const { name } = req.body;
-    if (!name) {
+    const { tags } = req.body;
+    if (!tags) {
         return res.status(400).json({ message: 'Invalid inputs' });
     }
     try {
         const dbRes = await prisma.product.findMany({
             where: {
-                name: name,
+                tags: {
+                    has: tags,
+                },
             },
             select: {
                 name: true,
                 description: true,
                 price: true,
+                id: true,
             },
         });
         return res.json({ message: 'product in db', products: dbRes });
     } catch (error) {
-        return res.status(500).json({ message: 'something went worng' });
+        return res.status(500).json({ message: 'something went worng here' });
     }
 }
+
+export async function order(req: Request, res: Response) {
+    const userEmail = req.user;
+    if (!req.body) {
+        return res.status(400).json({ message: 'Body not found' });
+    }
+    const { productId } = req.body;
+    if (!productId) {
+        return res.status(400).json({ message: 'Product id not found' });
+    }
+    let userId: number | undefined;
+    try {
+        const userRes = await prisma.user.findUnique({
+            where: {
+                email: userEmail,
+            },
+            select: {
+                id: true,
+            },
+        });
+        userId = userRes?.id;
+    } catch (error) {
+        return res.status(500).json({ message: 'something went worng here' });
+    }
+    if (userId) {
+        try {
+            const dbRes = await prisma.order.create({
+                data: {
+                    productId: productId,
+                    userId: userId,
+                },
+                select: {
+                    user: {
+                        select: {
+                            name: true,
+                            email: true,
+                        },
+                    },
+                    product: {
+                        select: {
+                            name: true,
+                            price: true,
+                            user: {
+                                select: {
+                                    name: true,
+                                    email: true,
+                                },
+                            },
+                        },
+                    },
+                },
+            });
+            return res.json({ message: 'order placed', order: dbRes });
+        } catch (error) {
+            console.log(error);
+            return res
+                .status(500)
+                .json({ message: 'something went worng here' });
+        }
+    } else {
+        return res.status(500).json({ message: 'something went worng here' });
+    }
+}
+
--- server/src/routes/productRouter.ts
+++ server/src/routes/productRouter.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { listProduct, seeProducts } from '../controller/product';
+import { listProduct, order, seeProducts } from '../controller/product';
 import { userAuth } from '../middleware/userAuth';
 const router = express.Router();
 
 router.post('/listproduct', userAuth,  listProduct);
 router.get('/products',seeProducts)
+router.post('/order',userAuth,order)
 
 export default router
